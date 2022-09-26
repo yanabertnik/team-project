e1cb4072042093e1d9d531e535cc64465e03c02f
yanabertnik <108129520+yanabertnik@users.noreply.github.com>
2022-09-26 06:05:27 +0000

    Deploying to gh-pages from @ yanabertnik/team-project@69cfc27162143ca11439e853252f92574436a979 🚀

---
+++ contacts.8b58f0d7.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector(".mobile__open"),closeMenuBtn:document.querySelector(".menu-close"),menu:document.querySelector(".mob-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=contacts.8b58f0d7.js.map
---
+++ contacts.f064a134.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".mobile__open"),closeMenuBtn:document.querySelector(".menu-close"),menu:document.querySelector(".mob-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=contacts.f064a134.js.map
